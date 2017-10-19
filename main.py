4ef230bb7ad3f74393e69a9acabbc3722f9eed55
skullking419 <32721744+skullking419@users.noreply.github.com>
2017-10-19 13:39:14 -0400

    further work on room.py, several spritesheets for experimentation,nothing crazy. yet.

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ pygame.init()
 
 #the actual layout of each room is encoded as a list of strings, where each char represents a tile
 # these will be looked up in the tileset
-testMapCode = ['fffff', 'fffff', 'fffff', 'fffff']
+testMapCode = ['fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff']
 outerBounds = ((0, len(testMapCode[0])), (0, len(testMapCode)))
 print(outerBounds)
 
-disp = pygame.display.set_mode((1280, 768))
+disp = pygame.display.set_mode((1560, 1000))
 
 import room
 
@@ -22,6 +22,13 @@ while (running):
         if event.type == pygame.QUIT:
             running = False
 
+    key = pygame.key.get_pressed()
+    if key[pygame.K_w]:
+        pass
+
     disp.blit(testRoom.fSurf, (0,0))
 
+
     pygame.display.update()
+
+
