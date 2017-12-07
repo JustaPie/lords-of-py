0a287228835735d269ad9499e77759ebc2f2812c
Mocut <32436359+Mocut@users.noreply.github.com>
2017-12-06 21:19:58 -0500

    Add files via upload
    Added working fog and picture for it

--- main.py
+++ main.py
@@ -1,46 +1,138 @@
-
 import pygame
-import sys
+
+#this first bit is just useful constants
+red = (255, 0, 0)
+blue = (0, 0, 255)
+green = (0, 255, 0)
+
+plyr_loc = (100, 100)
+nme1_loc = (500, 500)
+fps = 128
+screen_size = (1600, 1100)
 
 pygame.init()
 
-#the actual layout of each room is encoded as a list of strings, where each char represents a tile
-# these will be looked up in the tileset
-testMapCode = ['fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff', 'fffffffffffffff']
-outerBounds = ((0, len(testMapCode[0])), (0, len(testMapCode)))
-print(outerBounds)
+print('correct test')
 
-disp = pygame.display.set_mode((1560, 1000))
+disp = pygame.display.set_mode(screen_size)
 
-import room
+bkgd = pygame.image.load("Fog.png").convert_alpha()
+scroll = 0
 
-testRoom = room.room(testMapCode)
 
 import player
-pc = player.player((50,50))
-npc1 = player.baddy((500,100))
-npc2 = player.baddy((900,475))
-npc3 = player.baddy((654,346))
-npc4 = player.baddy((589,251))
-npc5 = player.baddy((123,97))
+import new_room
+import room
+import missiles
+import enemies
+import overlays
+
+test_room_code = ['ffffffffffffffff','fffffffffffffffff','fffffffffffffffff',
+                  'fffffffffffffffff','fffffffffffffffff','fffffffffffffffff',
+                  'fffffffffffffffff','fffffffffffffffff','fffffffffffffffff',
+                  'fffffffffffffffff', 'fffffffffffffffff']
 
 
-allSprites = pygame.sprite.Group(pc, npc1)
-badguys = pygame.sprite.Group(npc1, npc2, npc3, npc4, npc5)
+test_room = room.room(test_room_code)
+
+#test_room = new_room((1600,1100), 42, theme.fog_theme, 12, True)
+
+
+
+
+pc = player.player(plyr_loc)
+HUD = overlays.hud(pc, disp)
+hp = overlays.healthbar(pc, HUD)
+
+test_room.addPlayer(pc)
+
+bumper = enemies.bouncer((600, 600))
+
+test_room.enemies.add(bumper)
+
+#test_room.overlays.add(hp)
+
+'''
+fleye1 = enemies.fleye((500, 500))
+test_room.enemies.add(fleye1)
+fleye1.set_target(pc)
+
+''''''
+fleye2 = enemies.fleye((400, 400))
+test_room.enemies.add(fleye2)
+fleye2.set_target(pc)
+
+fleye3 = enemies.fleye((600, 600))
+test_room.enemies.add(fleye3)
+fleye3.set_target(pc)
+'''
 
 running = True
 while (running):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            pass
 
-    disp.blit(testRoom.fSurf, (0,0))
-    disp.blit(pc.image, pc.move())
-    #disp.blit(npc.portrait, npc.move())
-    allSprites.draw(disp)
-    badguys.draw(disp)
+    disp.blit(test_room.fSurf, (0,0))
 
+    test_room.player.update(test_room)
+    test_room.playerProjectiles.update(test_room)
+
+    test_room.enemies.update(test_room)
+    test_room.enemyProjectiles.update(test_room)
+
+    test_room.allSprites.draw(disp)
+    test_room.allProjectiles.draw(disp)
+
+    test_room.inactivePlayerProjectiles.update(test_room)
+    test_room.inactivePlayerProjectiles.draw(disp)
+
+    test_room.nme_overlays.draw(disp)
+
+    HUD.update(test_room)
+    HUD.draw(disp)
+
+    #player_hud.update()
+    #player_hud.show(disp)
+
+    for x in test_room.player:
+        pygame.draw.rect(disp, green, x.rect, 4)
+    for x in test_room.player:
+        pygame.draw.rect(disp, red, x.hitbox, 4)
+
+    for y in test_room.enemies:
+        pygame.draw.rect(disp, blue, y.rect, 8)
+    for y in test_room.enemies:
+        pygame.draw.rect(disp, red, y.hitbox, 4)
+        for z in y.hitboxes:
+            pygame.draw.rect(disp, green, z, 5)
+
+    for z in test_room.allProjectiles:
+        pygame.draw.rect(disp, red, z.rect, 7)
+    for z in test_room.allProjectiles:
+        pygame.draw.rect(disp, green, z.hitbox, 4)
+
+
+
+    test_room.update()
+
+    disp.blit(bkgd, (scroll, 0))
+    
+    rel = scroll % bkgd.get_rect().width
+    disp.blit(bkgd, (rel - bkgd.get_rect().width, 0))
+    
+    if rel < 1600:
+        disp.blit(bkgd, (rel,0))
+
+    scroll += 1
+
+    pygame.draw.line(disp, (205,205,205), (rel, 0), (rel, 1100), 1)
+
+    clock = pygame.time.Clock()
+    msElapsed = clock.tick(fps)
 
     pygame.display.update()
 
-
+    pygame.event.pump()
